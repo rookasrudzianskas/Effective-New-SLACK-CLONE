b79f2fcd938350d75792730fc78e3ceb08de93c1
Rokas <rookas.rudzenskas@gmail.com>
2021-05-06 16:56:45 +0300

    ROkas

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCWBZp-3Mo2iIDtr8R5wdvWX-eZp0NpmJ4",
+    authDomain: "slack-clone-cbc0b.firebaseapp.com",
+    projectId: "slack-clone-cbc0b",
+    storageBucket: "slack-clone-cbc0b.appspot.com",
+    messagingSenderId: "310594716767",
+    appId: "1:310594716767:web:fefa65d0dd7b671cd987ea",
+    measurementId: "G-7FPXDZWFWB"
+};
