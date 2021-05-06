6c9e0ebf97bff6428e2f3a8edb1c1e4cd5b4d0dc
Rokas <rookas.rudzenskas@gmail.com>
2021-05-06 09:14:42 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,21 @@ const Header = () => {
     return (
         <div className="header">
             <h1>I am the header</h1>
+
+            <div className="header__left">
+            {/*    Avatarr for logged in user   */}
+            {/*    Time Icon here   */}
+            </div>
+
+            <div className="header__search">
+            {/*    Search Icon  */}
+            {/*    input    */}
+            </div>
+
+            <div className="header__right">
+            {/*     help icon   */}
+
+            </div>
         </div>
     );
 };
