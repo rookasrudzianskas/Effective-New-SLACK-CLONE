bab77ab85fa32ee78117b07507b797a826171c8a
Rokas <rookas.rudzenskas@gmail.com>
2021-05-06 09:07:42 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <h1>Hello 🚀</h1>
 
+        <Header />
     {/*    Header   */}
 
     {/*    Sidebar  */}
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./styles/Header.css";
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>I am the header</h1>
+        </div>
+    );
+};
+
+export default Header;
