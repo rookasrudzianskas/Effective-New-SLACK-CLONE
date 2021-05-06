f8ccff2f9f26ee8005f8315baf69933d032da75e
Rokas <rookas.rudzenskas@gmail.com>
2021-05-06 22:00:42 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import './App.css';
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div className="app">
+        <Router>
+             <Header />
+        {/*    Header   */}
 
-        <Header />
-    {/*    Header   */}
         <div className="app__body">
             <Sidebar />
+            <Switch>
+
+                <Route path="/room/:roomId">
+                    <Chat />
+                </Route>
+
+                <Route path="/">
+                    <h1>Welcome</h1>
+                </Route>
+
+            </Switch>
             {/*    Sidebar  */}
 
             {/*    React router -> Chat screen  */}
         </div>
 
-
+        </Router>
     </div>
   );
 }
---
+++ src/components/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles/Chat.css";
+
+const Chat = () => {
+    return (
+        <div>
+                <h1>This is chat</h1>
+        </div>
+    );
+};
+
+export default Chat;
