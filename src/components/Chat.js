62cbae0d0146dfaf266111e67c915f39de9ee9d9
Rokas <rookas.rudzenskas@gmail.com>
2021-05-06 22:04:44 +0300

    Rokas

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./styles/Chat.css";
+import { useParams } from "react-router-dom";
 
 const Chat = () => {
+
+    // getting the room id parameter
+    // this room id is the /: what we have wrote in the url in the app.js, it must be the same, because from there it gets that id
+    const { roomId } = useParams();
+
     return (
-        <div>
-                <h1>This is chat</h1>
+        <div className="chat">
+                <h1>This is chat {roomId} room</h1>
         </div>
     );
 };
