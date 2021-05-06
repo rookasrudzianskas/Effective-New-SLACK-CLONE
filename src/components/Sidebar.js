1979c276c65f0c934f99d272a732de0291a6023c
Rokas <rookas.rudzenskas@gmail.com>
2021-05-06 16:12:31 +0300

    Rokas

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import "./styles/Sidebar.css";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from "@material-ui/icons/Create";
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            
+            <div className="sidebar__header">
+                <div className="sidebar__info">
+                <h2>Brainlyemails & CONNEXIOUS Startups</h2>
+                <h3>
+                    <FiberManualRecordIcon />
+                    Rokas Rudzianskas
+                </h3>
+                </div>
+                <CreateIcon />
+            </div>
         </div>
     );
 };
