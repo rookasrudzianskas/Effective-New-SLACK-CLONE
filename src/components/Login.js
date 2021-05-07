659f17ea9a86edb60ce3a3a6a66167f574beafb7
Rokas <rookas.rudzenskas@gmail.com>
2021-05-07 07:14:47 +0300

    Rokas

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import "./styles/Login.css";
 import { Button } from "@material-ui/core";
+import {auth, provider} from "../firebase";
 
 const Login = () => {
+
+    const signIn = (e) => {
+        // we handle the login with google in here
+        e.preventDefault();
+        // use the auth module, and signIn with Google, byt passing google as the provider and logging out the result
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result)
+        })
+            .catch((error) => {
+                alert(error.message)
+            });
+    }
+
+
     return (
         <div className="login">
             <div className="login__container">
@@ -10,7 +25,7 @@ const Login = () => {
 
                 <h1>Sign in to Brainlyemails & CONNEXIOUS HQ</h1>
                 <p>connexious.rokashq.com</p>
-                <Button>Sign In With Google</Button>
+                <Button onClick={signIn}>Sign In With Google</Button>
             </div>
 
         </div>
