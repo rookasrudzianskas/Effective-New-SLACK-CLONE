bfd02076ca19d01fde8adb880e8f2d1c7550bc7d
Rokas <rookas.rudzenskas@gmail.com>
2021-05-07 07:23:33 +0300

    Rokas

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -5,9 +5,13 @@ export const StateContext = createContext();
 
 // always copy it
 export const StateProvider = ({ reducer, initialState, children }) => (
+    // reducer listens for any kind of actions, youshoot to the data layer. reducer is the place, where listening for event happens
+    // initialState is basically how the data layer looks like, before we do anything about it
     <StateContext.Provider value={useReducer(reducer, initialState)}>
         {/* is our app, which is wrappend by StateProvider, which is out datalayer, to be able to access any info there*/}
         {children}
     </StateContext.Provider>
 );
 
+// this is how we access the data layer inside all the other components
+export const useStateValue = () => useContext(StateContext);
