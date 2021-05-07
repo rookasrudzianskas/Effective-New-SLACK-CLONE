3636bca5406db983da32edf0a4be9bd6ab50ef19
Rokas <rookas.rudzenskas@gmail.com>
2021-05-07 05:47:02 +0300

    Rokas

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import "./styles/SidebarOption.css";
+import { useHistory } from "react-router-dom";
 
 const SidebarOption = ({Icon, title, addChannelOption, id}) => {
+    // use history tracks where we go with and pushes to the history state all the data
+    const history = useHistory();
+    // so this one is going to fire if we click on the add channel and there is id of the channel
+    // so basically we have the addCHanenelOPtion is on the last icon addChannel option
+    // in the div we check if we have addCHannelOption so we do some stuff.
+    // In this case select if we do not have the addChannelOption, we just fire select channel
+    // if the channel has id, so we go to the /room/, and tka ethe id to the last url keyword
+    const selectChannel = () => {
+        if(id) {
+            // next page as the redirect
+            history.push(`/room/${id}`)
+        } else {
+            // next page, just title
+            history.push('title')
+        }
+    };
+
+    const addChannel = () => {
+
+    };
+
     return (
         <div className="sidebarOption" onClick={addChannelOption ? addChannel : selectChannel}>
             {/*rendering the icon*/}
